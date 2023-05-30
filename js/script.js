f8d16253cb9e4097ccdda6ac4f6eebdac6e05341
Jordan <jordanwenger11@gmail.com>
2023-05-30 07:27:33 -0400

    added custom alert when search is empty

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 const global = {
   currentPage: window.location.pathname,
+  search: {
+    term: '',
+    type: '',
+    page: 1,
+    totalPages: 1
+  },
+  api: {
+    apiKey: '1b54533b0ee26bfbf56551b963d2f5de',
+    apiURL: "https://api.themoviedb.org/3/"
+  }
 };
 
 async function displayPopularMovies() {
@@ -139,7 +149,7 @@ async function displayShowDetails() {
   console.log(showId);
 
   const show = await fetchAPIData(`tv/${showId}`);
-  console.log(show);
+  console.log(show)
   // Overlay Background Image
   displayBackgroundImage("tv", show.backdrop_path);
 
@@ -220,6 +230,23 @@ function displayBackgroundImage(type, backgroundPath) {
   }
 }
 
+// Search Movies/Shows
+
+async function search() {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+
+  global.search.type = urlParams.get('type');
+  global.search.term = urlParams.get('search-term');
+
+  if(global.search.term !== '' && global.search.term !== null) {
+    const results = await searchAPIData();
+    console.log(results)
+  } else {
+    showAlert('Please enter a search term');
+  }
+}
+
 // Display Slider Movies
 async function displaySlider() {
   const { results } = await fetchAPIData("movie/now_playing");
@@ -270,13 +297,31 @@ function initSwiper() {
 async function fetchAPIData(endpoint) {
   // Register your key at https://www.themoviedb.org/api and enter here
   // Only use for development or very small projects. You should store your key and make a requests from a server
-  const API_KEY = "1b54533b0ee26bfbf56551b963d2f5de";
-  const API_URL = "https://api.themoviedb.org/3/";
+  const API_KEY = global.api.apiKey;
+  const API_URL = global.api.apiURL;
 
   showSpinner();
 
   const response = await fetch(
     `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
+  );
+
+  hideSpinner();
+
+  const data = await response.json();
+
+  return data;
+}
+
+// Make Request To Search
+async function searchAPIData() {
+  const API_KEY = global.api.apiKey;
+  const API_URL = global.api.apiURL;
+
+  showSpinner();
+
+  const response = await fetch(
+    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`
   );
 
   hideSpinner();
@@ -304,6 +349,16 @@ function highlightActiveLink() {
   });
 }
 
+// Show Alert
+function showAlert(message, className) {
+  const alertEl = document.createElement('div');
+  alertEl.classList.add('alert', className)
+  alertEl.appendChild(document.createTextNode(message));
+  document.querySelector('#alert').appendChild(alertEl);
+
+  setTimeout(() => alertEl.remove(), 4000);
+}
+
 function addCommasToNumber(number) {
   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
@@ -325,7 +380,7 @@ function init() {
       displayShowDetails();
       break;
     case "/search.html":
-      console.log("Search");
+      search();
       break;
   }
 
